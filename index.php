004a36a6def762b7c208b35f175290822eb1a78e
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-04-23 15:07:57 -0300

    add to requires

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//mensagem
+include_once "layout/mensagem.php";
+//fazendo a requisição do cabechalho html
+require_once 'layout/cabecalho.html';
+//incluindo conexao com banco de dados
+include_once 'database/banco.php';
+
+?>
+
+
+
+<?php
+require_once 'layout/rodape.html'>;
+?>
